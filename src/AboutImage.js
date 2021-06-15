aa5296f33c2829abe93c9589ebe856bae48465ab
mide358 <iyimide.adegunloye@gmail.com>
2021-06-15 12:38:50 +0100

    job title

--- src/AboutImage.js
+++ src/AboutImage.js
@@ -1,17 +1,18 @@
-import Portrait from './images/portrait2.jpg';
-import React from 'react';
-import Card from 'react-bootstrap/Card';
+import Portrait from "./images/portrait2.jpg";
+import React from "react";
+import Card from "react-bootstrap/Card";
 
 const AboutImage = () => {
   return (
-   <Card className="aboutimgcard" style={{height:'25rem', width:'25rem' }}>
-
-
-       <Card.Img  src={Portrait}className="about-img-top mypix mx-auto d-block" alt="..."  />
-    <Card.Text className="myjob">Software Developer</Card.Text>
-
+    <Card className="aboutimgcard" style={{ height: "25rem", width: "25rem" }}>
+      <Card.Img
+        src={Portrait}
+        className="about-img-top mypix mx-auto d-block"
+        alt="..."
+      />
+      <Card.Text className="myjob">Software Developer</Card.Text>
     </Card>
-   );
-}
+  );
+};
 
 export default AboutImage;
